46bd7926df77932bf3e906d04865fd3dd06c2179
Meganster <ppro100takk@mail.ru>
2018-03-31 20:38:30 +0300

    Repair 5

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -3,9 +3,10 @@ import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import services.Application;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class ApplicationTest {
 
     @Test
