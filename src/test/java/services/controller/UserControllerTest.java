cfcc39263743efede767bfe75f93968ef91496ac
Meganster <ppro100takk@mail.ru>
2018-04-05 12:48:06 +0300

    Determinate tests, add tests for sessions

--- src/test/java/services/controller/UserControllerTest.java
+++ src/test/java/services/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package services.controller;
 
 
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +11,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import services.dao.UserDAO;
-import services.model.ScoreBoard;
 import services.model.ServerResponse;
 import services.model.User;
 
@@ -20,7 +18,8 @@ import services.model.User;
 import java.util.ArrayList;
 
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
@@ -29,6 +28,7 @@ import static org.mockito.Mockito.when;
 public class UserControllerTest {
     private static final String LOGIN = "example_login";
     private static final String LOGIN_FIRST = "example_login_1";
+    private static final String NICKNAME = "example_nickname";
     private static final String PASSWORD = "example_password";
     private static final String WRONG_PASSWORD = "wrong_password";
     private static final HttpHeaders REQUEST_HEADERS = new HttpHeaders();
@@ -40,6 +40,7 @@ public class UserControllerTest {
     private TestRestTemplate restTemplate;
 
 
+    @SuppressWarnings("Duplicates")
     @BeforeClass
     public static void setHttpHeaders() {
         final ArrayList<String> origin = new ArrayList<>();
@@ -63,20 +64,6 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testGetUserRequiresLogin() {
-        final ResponseEntity<User> getUserResponse = restTemplate.
-                getForEntity("/getUser", User.class);
-        assertEquals(HttpStatus.UNAUTHORIZED, getUserResponse.getStatusCode());
-    }
-
-    @Test
-    public void testUpdateAvatarRequiresLogin() {
-        final ResponseEntity<User> updateAvatarResponse = restTemplate.
-                getForEntity("/updateAvatar", User.class);
-        assertEquals(HttpStatus.UNAUTHORIZED, updateAvatarResponse.getStatusCode());
-    }
-
-    @Test
     public void testGetAvatarNotFound() {
         final ResponseEntity<User> getAvatarResponse = restTemplate.
                 getForEntity("/avatar/defaultуSuper.gif", User.class);
@@ -84,18 +71,8 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testGetLeadersEmpty() {
-        final HttpEntity<User> requestEntity = new HttpEntity<>(REQUEST_HEADERS);
-        final ResponseEntity<ScoreBoard> response = restTemplate.getForEntity("/scoreboard/1",
-                ScoreBoard.class, requestEntity);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        Assert.assertTrue(response.getBody().getScoreBoard().isEmpty());
-    }
-
-    @Test
     public void testRegisterRequiresEmail() {
-        final User userToRegister = new User(LOGIN, "", PASSWORD, 0);
+        final User userToRegister = new User(NICKNAME, "", PASSWORD, 0);
         final HttpEntity<User> requestEntity = new HttpEntity<>(userToRegister, REQUEST_HEADERS);
 
         final ResponseEntity<ServerResponse> response = restTemplate.postForEntity("/register",
@@ -108,8 +85,7 @@ public class UserControllerTest {
 
     @Test
     public void testRegisterSameUserExist() {
-        final User existUser = new User(LOGIN, LOGIN, PASSWORD, 0);
-        when(userService.getUser(LOGIN)).thenReturn(existUser);
+        when(userService.register(any())).thenReturn(false);
 
         final User userToRegister = new User(LOGIN_FIRST, LOGIN, PASSWORD, 0);
         final HttpEntity<User> requestEntity = new HttpEntity<>(userToRegister, REQUEST_HEADERS);
@@ -122,24 +98,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testLoginRequiresEmail() {
-        final User userToLogin = new User(LOGIN, "", PASSWORD, 0);
-        final HttpEntity<User> requestEntity = new HttpEntity<>(userToLogin, REQUEST_HEADERS);
-
-        final ResponseEntity<ServerResponse> response = restTemplate.postForEntity("/login",
-                requestEntity, ServerResponse.class);
-
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("Empty email", response.getBody().getMessage());
-    }
-
-
-    @Test
     public void testLoginWrongPassword() {
-        final User existUser = new User(LOGIN, LOGIN, PASSWORD, 0);
-        when(userService.getUser(LOGIN)).thenReturn(existUser);
+        when(userService.login(any())).thenReturn(false);
 
-        final User userToLogin = new User(LOGIN, LOGIN, WRONG_PASSWORD, 0);
+        final User userToLogin = new User(NICKNAME, LOGIN, WRONG_PASSWORD, 0);
         final HttpEntity<User> requestEntity = new HttpEntity<>(userToLogin, REQUEST_HEADERS);
 
         final ResponseEntity<ServerResponse> response = restTemplate.postForEntity("/login",
@@ -150,23 +112,14 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testUpdateUserrRequiresLogin() {
-        final User userToUpdate = new User(LOGIN, LOGIN, PASSWORD, 0);
-        final HttpEntity<User> requestEntity = new HttpEntity<>(userToUpdate, REQUEST_HEADERS);
+    public void testLoginRequiresEmail() {
+        final User userToLogin = new User(NICKNAME, "", PASSWORD, 0);
+        final HttpEntity<User> requestEntity = new HttpEntity<>(userToLogin, REQUEST_HEADERS);
 
-        final ResponseEntity<ServerResponse> response = restTemplate.exchange(
-                "/updateUser", HttpMethod.PUT, requestEntity, ServerResponse.class);
-
-        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
-        assertEquals("You are not login", response.getBody().getMessage());
-    }
-
-    @Test
-    public void testLogoutRequiresLogin() {
-        final ResponseEntity<ServerResponse> response = restTemplate.exchange(
-                "/logout", HttpMethod.DELETE, null, ServerResponse.class);
+        final ResponseEntity<ServerResponse> response = restTemplate.postForEntity("/login",
+                requestEntity, ServerResponse.class);
 
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("You are not login", response.getBody().getMessage());
+        assertEquals("Empty email", response.getBody().getMessage());
     }
 }
